0fa8a74bdfca7175ebf8c2f41170da50263f7d15
Jeremie <jeremie.kaufmann@gmail.com>
2021-11-08 17:41:21 +0100

    Try catch pour afficher le message de l'exception lancée

--- index.php
+++ index.php
@@ -4,18 +4,20 @@ declare(strict_types = 1);
 require_once('./Jeu.class.php');
 require_once('./Joueur.class.php');
 
+try {
+    // Déclaration du jeu
+    $jeu = new Jeu;
 
+    // Ajout des joueurs
+    $jeu->creerJoueur("Roger Rabbit");
+    $jeu->creerJoueur("Eddie Valliant");
 
-// Déclaration du jeu
-$jeu = new Jeu;
-// Ajout des joueurs
-$jeu->creerJoueur("Roger Rabbit");
-$jeu->creerJoueur("Eddie Valliant");
+    // lancement de la partie
+    $jeu->lancerJeu();
 
-
-// lancement de la partie
-$jeu->lancerJeu();
-
-// Affichage du nom du gagnant
-echo "Résultat : " . $jeu->getResultat();
+    // Affichage du nom du gagnant
+    echo "Résultat : " . $jeu->getResultat();
+} catch (Exception $e) {
+    echo "Une exception a été levée durant le jeu : " . $e->getMessage();
+}
 
